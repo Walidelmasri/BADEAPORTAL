c4da6964eb9c6462b3c9cfee10aa14b9baecfe5d
Walidelmasri <waldaexist@gmail.com>
2026-05-03 12:02:38 +0300

    changed logo

--- obj/Debug/net9.0/BADEAPORTAL.AssemblyInfo.cs
+++ obj/Debug/net9.0/BADEAPORTAL.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BADEAPORTAL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c7a736e97d7483603c9d2db128497f2bea7d4c65")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5d3ae58a2c7ad676defe03a447de90271de5c85d")]
 [assembly: System.Reflection.AssemblyProductAttribute("BADEAPORTAL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BADEAPORTAL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
